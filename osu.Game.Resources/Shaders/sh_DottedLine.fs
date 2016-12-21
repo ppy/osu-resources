0a6c6d14e123c12d988c6a7e141f3dfcaaba19ba
smoogipooo <smoogipooo@gmail.com>
2016-12-21 18:34:32 +1030

    Add visualiser line shader.

---
+++ osu.Game.Resources/Shaders/sh_DottedLine.fs
@@ -0,0 +1,41 @@
+#ifdef GL_ES
+    precision mediump float;
+#endif
+
+#include "sh_Utils.h"
+
+varying vec4 v_Colour;
+
+uniform vec2 g_Position;
+uniform vec2 g_Size;
+
+uniform float g_B;
+uniform float g_C;
+uniform float g_D;
+
+uniform float g_StrokeWidth;
+uniform float g_StrokeHeight;
+uniform float g_Separation;
+
+void main(void)
+{
+	float xVisible = mod(gl_FragCoord.x, g_StrokeWidth + g_Separation);
+
+	if (xVisible > g_StrokeWidth)
+	{
+		gl_FragColor = vec4(0.0);
+		return;
+	}
+
+	// Normalized 
+	float normalizedY = gl_FragCoord.y - g_Position.y;
+	float yVisible = abs(normalizedY - (g_Size.y / 2.0 * sin(g_B / g_Size.x * gl_FragCoord.x + g_C) + g_Size.y / 2.0));
+
+	if (yVisible > g_StrokeHeight / 2.0)
+	{
+		gl_FragColor = vec4(0.0);
+		return;
+	}
+
+	gl_FragColor = toSRGB(v_Colour);
+}
