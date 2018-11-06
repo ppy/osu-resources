a7e3003b3bc7f96d8f9cbbe955cb227cc2f90bc7
jorolf <jorolf@gmx.de>
2018-11-07 00:47:35 +0100

    add flashlight shader

---
+++ osu.Game.Resources/Shaders/sh_Flashlight.fs
@@ -0,0 +1,21 @@
+﻿#ifdef GL_ES
+    precision mediump float;
+#endif
+
+varying vec2 v_Position;
+
+uniform vec2 mousePos;
+uniform float flashlightSize;
+
+const float smoothness = 0.1;
+
+void main(void)
+{
+    vec2 diff = mousePos - v_Position;
+
+    float dist = sqrt(diff.x * diff.x + diff.y * diff.y);
+
+    float smoothnessRange = flashlightSize * smoothness;
+
+    gl_FragColor = vec4(0.0, 0.0, 0.0, smoothstep(flashlightSize, flashlightSize + smoothnessRange, dist));
+}
