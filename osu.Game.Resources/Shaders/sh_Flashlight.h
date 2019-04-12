801c6efa966673d8198a5e8f42e4d6b7adb0ae03
David Zhao <nyquill@ppy.sh>
2019-04-12 10:50:10 +0900

    Add lightness to flashlight shader for dimming

--- osu.Game.Resources/Shaders/sh_Flashlight.h
+++ osu.Game.Resources/Shaders/sh_Flashlight.h
@@ -8,11 +8,13 @@ varying vec4 v_Colour;
 uniform vec2 flashlightPos;
 uniform vec2 flashlightSize;
 
+uniform float flashlightLightness;
+
 const float smoothness = 1.1;
 
 vec4 getColourAt(vec2, vec2, vec4);
 
 void main(void)
 {
-    gl_FragColor = getColourAt(flashlightPos - v_Position, flashlightSize, v_Colour);
+    gl_FragColor = mix(vec4(0.0, 0.0, 0.0, 1.0), getColourAt(flashlightPos - v_Position, flashlightSize, v_Colour), flashlightLightness);
 }
