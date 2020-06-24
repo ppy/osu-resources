a0942615590a5798733072f7def1cce2192eb983
jorolf <jorolf@gmx.de>
2020-06-24 15:01:17 +0200

    format shader file

--- osu.Game.Resources/Shaders/sh_HueAnimation.fs
+++ osu.Game.Resources/Shaders/sh_HueAnimation.fs
@@ -19,8 +19,8 @@ vec3 rgb2hsv(vec3 c)
 void main(void) 
 {
     vec4 colour = texture2D(m_Sampler, v_TexCoord, -0.9);
-    
+
     vec3 hsv = rgb2hsv(colour.rgb);
-    
+
     gl_FragColor = hsv.x < progress ? vec4(v_Colour.rgb, v_Colour.a * colour.a) : vec4(0);
 }
