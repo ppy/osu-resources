061e419dbcd4237656b1c84336c24632736328ea
Dean Herbert <pe@ppy.sh>
2022-10-08 21:32:08 +0900

    Fix broken shader

--- osu.Game.Resources/Shaders/sh_RectangularFlashlight.fs
+++ osu.Game.Resources/Shaders/sh_RectangularFlashlight.fs
@@ -4,7 +4,7 @@ lowp vec4 getColourAt(highp vec2 diff, highp vec2 size, lowp vec4 originalColour
 {
     diff = abs(diff);
 
-    lowp float alpha = length(smoothstep(size, size * smoothness, diff));
+    lowp float alpha = length(smoothstep(size, size * flashlightSmoothness, diff));
 
     return originalColour * vec4(1.0, 1.0, 1.0, alpha);
 }
