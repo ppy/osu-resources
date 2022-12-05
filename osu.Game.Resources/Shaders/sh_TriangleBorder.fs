feba13513064b534068918b76d983133b5c69e11
Andrei Zavatski <megaman9919@gmail.com>
2022-12-05 22:26:56 +0300

    Remove texture sampling from TriangleBorder shader

--- osu.Game.Resources/Shaders/sh_TriangleBorder.fs
+++ osu.Game.Resources/Shaders/sh_TriangleBorder.fs
@@ -5,7 +5,6 @@
 
 varying highp vec2 v_TexCoord;
 
-uniform lowp sampler2D m_Sampler;
 uniform mediump float thickness;
 uniform highp float texelSize;
 
@@ -47,7 +46,7 @@ void main(void)
 
     lowp float alpha = dst < texelSize ? dst / texelSize : smoothstep(texelSize, 0.0, dst - thickness);
 
-    lowp vec4 col = getRoundedColor(texture2D(m_Sampler, v_TexCoord), v_TexCoord);
+    lowp vec4 col = getRoundedColor(vec4(1.0), v_TexCoord);
 
     gl_FragColor = vec4(col.rgb, col.a * alpha);
 }
