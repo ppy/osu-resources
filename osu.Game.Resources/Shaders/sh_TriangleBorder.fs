64d94934327aeb66f09b55a117e84e75f75b562b
Andrei Zavatski <megaman9919@gmail.com>
2023-09-28 23:18:51 +0300

    Remove excessive distance calculation by mirroring the space

--- osu.Game.Resources/Shaders/sh_TriangleBorder.fs
+++ osu.Game.Resources/Shaders/sh_TriangleBorder.fs
@@ -16,11 +16,7 @@ layout(location = 0) out vec4 o_Colour;
 highp float dstToLine(highp vec2 start, highp vec2 end, highp vec2 pixelPos)
 {
     highp float lineLength = distance(end, start);
-
-    if (lineLength < 0.001)
-        return distance(pixelPos, start);
-
-    highp vec2 a = (end - start) / lineLength;
+    highp vec2 a = (end - start) / lineLength; // Within the scope of this shader line length won't be 0
     highp vec2 closest = clamp(dot(a, pixelPos - start), 0.0, lineLength) * a + start; // closest point on a line from given position
     return distance(closest, pixelPos);
 }
@@ -28,18 +24,18 @@ highp float dstToLine(highp vec2 start, highp vec2 end, highp vec2 pixelPos)
 void main(void)
 {
     highp vec2 pixelPos = v_TexCoord / (v_TexRect.zw - v_TexRect.xy);
+    pixelPos.x = 0.5 - abs(pixelPos.x - 0.5); // Mirror coordinate space horizontally (0 -> 1 to 0 -> 0.5 -> 0)
 
     // return if outside the triangle
-    if (abs(pixelPos.x - 0.5) > 0.5 * pixelPos.y)
+    if (pixelPos.y < 1.0 - 2.0 * pixelPos.x)
     {
         o_Colour = vec4(0.0);
         return;
     }
 
-    highp float dst1 = 1.0 - pixelPos.y;
-    highp float dst2 = dstToLine(vec2(1.0), vec2(0.5, 0.0), pixelPos);
-    highp float dst3 = dstToLine(vec2(0.0, 1.0), vec2(0.5, 0.0), pixelPos);
-    highp float dst = min(min(dst1, dst2), dst3);
+    highp float dstToBottom = 1.0 - pixelPos.y;
+    highp float dstToSide = dstToLine(vec2(0.0, 1.0), vec2(0.5, 0.0), pixelPos);
+    highp float dst = min(dstToBottom, dstToSide);
 
     lowp float alpha = 0.0;
 
