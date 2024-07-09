002bdb5bd932b71fffd0b61e4da3214e05f0951e
Dan Balasescu <smoogipoo@smgi.me>
2024-07-09 12:13:36 +0900

    Fix flashlight dimming on sliders with Cinema mod

--- osu.Game.Resources/Shaders/sh_Flashlight.h
+++ osu.Game.Resources/Shaders/sh_Flashlight.h
@@ -18,5 +18,5 @@ void main(void)
     // todo: workaround for a SPIR-V bug (https://github.com/ppy/osu-framework/issues/5719)
     float one = g_WrapModeS >= 0 ? 1 : 0;
 
-    o_Colour = mix(getColourAt(flashlightPos - v_Position, flashlightSize, v_Colour), vec4(0.0, 0.0, 0.0, 1.0), flashlightDim) * one;
+    o_Colour = mix(getColourAt(flashlightPos - v_Position, flashlightSize, v_Colour), vec4(0.0, 0.0, 0.0, v_Colour.a), flashlightDim) * one;
 }
