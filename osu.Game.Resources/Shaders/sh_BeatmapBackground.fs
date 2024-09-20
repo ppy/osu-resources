e2c52c9d9601d16a94a825214bc620ed5012cb01
Uncomfy <dimabalabanokda@gmail.com>
2024-09-20 08:50:59 +0300

    Made beatmap background shader accept extra params

--- osu.Game.Resources/Shaders/sh_BeatmapBackground.fs
+++ osu.Game.Resources/Shaders/sh_BeatmapBackground.fs
@@ -7,6 +7,12 @@ layout(location = 2) in mediump vec2 v_TexCoord;
 layout(set = 0, binding = 0) uniform lowp texture2D m_Texture;
 layout(set = 0, binding = 1) uniform lowp sampler m_Sampler;
 
+layout(std140, set = 1, binding = 0) uniform m_BeatmapBackgroundParameters
+{
+    lowp float m_DimLevel;
+    lowp float m_DimColour;
+};
+
 layout(location = 0) out vec4 o_Colour;
 
 void main(void) 
