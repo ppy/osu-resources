57f59dc47b4bca6311ca6372dc3453d5dd1def20
Uncomfy <dimabalabanokda@gmail.com>
2024-09-20 11:32:42 +0300

    Made BeatmapBackground shader support colour as an m_DimColour input instead of a single float

--- osu.Game.Resources/Shaders/sh_BeatmapBackground.fs
+++ osu.Game.Resources/Shaders/sh_BeatmapBackground.fs
@@ -9,8 +9,8 @@ layout(set = 0, binding = 1) uniform lowp sampler m_Sampler;
 
 layout(std140, set = 1, binding = 0) uniform m_BeatmapBackgroundParameters
 {
+    lowp vec4 m_DimColour;
     lowp float m_DimLevel;
-    lowp float m_DimColour;
 };
 
 layout(location = 0) out vec4 o_Colour;
@@ -19,7 +19,5 @@ void main(void)
 {
     vec2 wrappedCoord = wrap(v_TexCoord, v_TexRect);
     o_Colour = getRoundedColor(wrappedSampler(wrappedCoord, v_TexRect, m_Texture, m_Sampler, -0.9), wrappedCoord);
-
-    vec4 dimColour = vec4(m_DimColour, m_DimColour, m_DimColour, 1.0);
-    o_Colour = mix(o_Colour, dimColour, m_DimLevel);
+    o_Colour = mix(o_Colour, m_DimColour, m_DimLevel);
 }
