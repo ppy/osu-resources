106b88976819cfee3209a09148d49591f50fc8a5
Uncomfy <dimabalabanokda@gmail.com>
2024-11-18 19:38:12 +0200

    Changed the name of the shader

--- osu.Game.Resources/Shaders/sh_ColouredDimmableTexture.fs
+++ osu.Game.Resources/Shaders/sh_ColouredDimmableTexture.fs
@@ -7,7 +7,7 @@ layout(location = 2) in mediump vec2 v_TexCoord;
 layout(set = 0, binding = 0) uniform lowp texture2D m_Texture;
 layout(set = 0, binding = 1) uniform lowp sampler m_Sampler;
 
-layout(std140, set = 1, binding = 0) uniform m_BeatmapBackgroundParameters
+layout(std140, set = 1, binding = 0) uniform m_DimParameters
 {
     lowp vec4 m_DimColour;
     lowp float m_DimLevel;
