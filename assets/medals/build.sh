82d768193ad95255db5da3005d79072f1b07a42c
Dean Herbert <pe@ppy.sh>
2026-06-18 02:17:17 +0900

    Add progress output and move all output to `output` subdir

--- assets/medals/build.sh
+++ assets/medals/build.sh
@@ -4,21 +4,24 @@
 
 set -eu
 
-mkdir -p client web tmp
+mkdir -p output/client output/web output/tmp
 
 colours=('purple' 'pink' 'red' 'bronze' 'silver' 'gold' 'blue' 'grey' 'green')
 offset='+10+4'
 
 for base_colour in "${colours[@]}"; do
-    magick -background none "base/medal-$base_colour.svg" $"tmp/client-$base_colour.png"
-    magick base/dropshadow.png -resize 408x439 -background none "base/medal-$base_colour.svg" -geometry "$offset" -composite "tmp/web-$base_colour.png"
+    echo "Processing $base_colour medals..."
+
+    magick -background none "base/medal-$base_colour.svg" $"output/tmp/client-$base_colour.png"
+    magick base/dropshadow.png -resize 408x439 -background none "base/medal-$base_colour.svg" -geometry "$offset" -composite "output/tmp/web-$base_colour.png"
     for file_path in "$base_colour"/*.svg; do
+        echo "* $file_path"
         file=$(basename "$file_path")
-        magick "tmp/client-$base_colour.png" -background none "$file_path" -composite -resize 385x417 "client/${file%.*}@2x.png"
-        magick "tmp/client-$base_colour.png" -background none "$file_path" -composite -resize 193x209 "client/${file%.*}.png"
-        magick "tmp/web-$base_colour.png" -background none "$file_path" -geometry "$offset" -composite -resize 248x248 "web/${file%.*}@2x.png"
-        magick "tmp/web-$base_colour.png" -background none "$file_path" -geometry "$offset" -composite -resize 111x119 "web/${file%.*}.png"
+        magick "output/tmp/client-$base_colour.png" -background none "$file_path" -composite -resize 385x417 "output/client/${file%.*}@2x.png"
+        magick "output/tmp/client-$base_colour.png" -background none "$file_path" -composite -resize 193x209 "output/client/${file%.*}.png"
+        magick "output/tmp/web-$base_colour.png" -background none "$file_path" -geometry "$offset" -composite -resize 248x248 "output/web/${file%.*}@2x.png"
+        magick "output/tmp/web-$base_colour.png" -background none "$file_path" -geometry "$offset" -composite -resize 111x119 "output/web/${file%.*}.png"
     done
 done
 
-rm -r tmp
+rm -r output/tmp
