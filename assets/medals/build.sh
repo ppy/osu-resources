6591b732170ea16e4e4ac8c745e3936fd8e190e6
Dean Herbert <pe@ppy.sh>
2026-06-17 19:10:09 +0900

    Better drop shadow

--- assets/medals/build.sh
+++ assets/medals/build.sh
@@ -8,9 +8,10 @@ mkdir -p client web tmp
 
 colours=('purple' 'pink' 'red' 'bronze' 'silver' 'gold' 'blue' 'grey' 'green')
 offset='+10+4'
+
 for base_colour in "${colours[@]}"; do
     magick -background none "base/medal-$base_colour.svg" $"tmp/client-$base_colour.png"
-    magick shadow/dropshadow.png -resize 408x439 -background none "base/medal-$base_colour.svg" -geometry "$offset" -composite "tmp/web-$base_colour.png"
+    magick base/dropshadow.png -resize 408x439 -background none "base/medal-$base_colour.svg" -geometry "$offset" -composite "tmp/web-$base_colour.png"
     for file_path in "$base_colour"/*.svg; do
         file=$(basename "$file_path")
         magick "tmp/client-$base_colour.png" -background none "$file_path" -composite -resize 385x417 "client/${file%.*}@2x.png"
@@ -19,4 +20,5 @@ for base_colour in "${colours[@]}"; do
         magick "tmp/web-$base_colour.png" -background none "$file_path" -geometry "$offset" -composite -resize 111x119 "web/${file%.*}.png"
     done
 done
+
 rm -r tmp
